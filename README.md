f2b911671dbdf3ed389bcc4fb8e5670a919ae18f
JoshuaMeza <jmeza@cubicode.mx>
2023-05-07 19:58:27 -0600

    Initial command

---
+++ README.md
@@ -0,0 +1,5 @@
+# Mantenimiento de Software - Zull
+
+## Reporte de errores
+
+En el caso de que encuentres algún problema con el juego, favor de reportarlo en el [Formulario de Reportes](https://forms.gle/oK7EM9Rsusb8ughQ6).
---
+++ test/CommandTests.java
@@ -0,0 +1,16 @@
+package test;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import src.*;
+
+public class CommandTests {
+    
+}
