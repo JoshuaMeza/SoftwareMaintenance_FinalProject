b1f71f0aa0fc38106a474142c177fe3fe7fee74b
JonathanGGB <56163813+JonathanGGB@users.noreply.github.com>
2023-05-09 15:51:50 -0600

    MZ-1-20230507

--- src/Game.java
+++ src/Game.java
@@ -4,6 +4,13 @@ import src.world.Handler;
 import src.world.JsonConcreteHandler;
 import src.world.XmlConcreteHandler;
 
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+
 /**
  *  This class is the main class of the "World of Zuul" application. 
  *  "World of Zuul" is a very simple, text based adventure game.  Users 
@@ -29,6 +36,8 @@ public class Game
     private Handler worldLoaderHandler;
     private Parser parser;
     private Room currentRoom;
+    private JSONObject jsonObject;
+    private String source_languaje_path;
         
     /**
      * Create the game and initialise its internal map.
@@ -50,7 +59,7 @@ public class Game
         this.currentRoom = worldLoaderHandler.handle();
         
         if (currentRoom == null) {
-            this.currentRoom = new Room("CONFIGURATION NOT FOUND!");
+            this.currentRoom = new Room(jsonObject.get("Not_found").toString());
         }
     }
 
@@ -59,8 +68,23 @@ public class Game
      */
     public void play() 
     {      
+        String inputLine;
         view.clear();
-
+        view.warningMsg(printLanguage());
+        inputLine = view.read("> "); 
+        while (true){
+            if (inputLine.equals("1") || inputLine.equals("2")){
+                if (inputLine.equals("1")){
+                    source_languaje_path = "./config/locales/en.json";
+                } else{
+                    source_languaje_path = "./config/locales/es.json";
+                }
+                jsonObject = readConfig();
+                break;
+            }
+            view.dangerMsg("Select one of the valid languajes numbers / Seleccione uno de los números de lenguaje válidos\n");
+            inputLine = view.read("> "); 
+        } 
         view.successMsg(printWelcome());
         printWhereAmI();
 
@@ -72,7 +96,7 @@ public class Game
             Command command = parser.getCommand();
             finished = processCommand(command);
         }
-        view.print("Thank you for playing. Good bye.\n");
+        view.print(jsonObject.get("Bye").toString());
         view.clear();
     }
 
@@ -80,27 +104,31 @@ public class Game
      * Print out the opening message for the player.
      */
     private String printWelcome(){
-        return "Welcome to the World of Zuul!\n" +
-                "World of Zuul is a new, incredibly boring adventure game.\n" +
-                "Type 'help' if you need help.\n";
+        return jsonObject.get("Welcome").toString();
+    }
+
+    private String printLanguage(){
+        return "Please select a language / Por favor selecciona un idioma\n" +
+                "1- English / Inglés.\n" +
+                "2- Spanish / Español.\n";
     }
 
     private void printWhereAmI()
     {
-        view.print("You are " + currentRoom.getDescription()+"\n");
-        view.print("Exits: \n");
+        view.print(jsonObject.get("You_are").toString() + currentRoom.getDescription()+"\n");
+        view.print(jsonObject.get("Exits").toString());
         
         if(currentRoom.northExit != null) {
-            view.warningMsg("north ");
+            view.warningMsg(jsonObject.get("North").toString());
         }
         if(currentRoom.eastExit != null) {
-            view.warningMsg("east ");
+            view.warningMsg(jsonObject.get("East").toString());
         }
         if(currentRoom.southExit != null) {
-            view.warningMsg("south ");
+            view.warningMsg(jsonObject.get("South").toString());
         }
         if(currentRoom.westExit != null) {
-            view.warningMsg("west ");
+            view.warningMsg(jsonObject.get("West").toString());
         }
         view.print("\n");
     }
@@ -115,16 +143,16 @@ public class Game
         boolean wantToQuit = false;
 
         if(command.isUnknown()) {
-            view.dangerMsg("I don't know what you mean...\n");
+            view.dangerMsg(jsonObject.get("Unknown").toString());
             return false;
         }
 
         String commandWord = command.getCommandWord();
-        if (commandWord.equals("help"))
+        if (commandWord.equals("help") || commandWord.equals("ayuda"))
             printHelp();
-        else if (commandWord.equals("go"))
+        else if (commandWord.equals("go") || commandWord.equals("ir"))
             goRoom(command);
-        else if (commandWord.equals("quit"))
+        else if (commandWord.equals("quit") || commandWord.equals("quitar"))
             wantToQuit = quit(command);
 
         return wantToQuit;
@@ -139,11 +167,8 @@ public class Game
      */
     private void printHelp() 
     {
-        view.warningMsg("You are lost. You are alone. You wander\n" + 
-        "around at the university.\n"+
-        "\n"+
-        "Your command words are:\n");
-        view.successMsg("   go quit help");
+        view.warningMsg(jsonObject.get("Help").toString());
+        view.successMsg(jsonObject.get("Commands").toString());
     }
 
     /** 
@@ -153,43 +178,43 @@ public class Game
     private void goRoom(Command command) 
     {
         if(!command.hasSecondWord()){
-            System.out.println("Invalid command. You must specify where to go");
+            System.out.println(jsonObject.get("Invalid_command").toString());
             return;
         }
         String direction = command.getSecondWord();
 
         // Try to leave current room.
         Room nextRoom = null;
-        if(direction.equals("north")) {
+        if(direction.equals("north") || direction.equals("norte")) {
             nextRoom = currentRoom.northExit;
         }
-        if(direction.equals("east")) {
+        if(direction.equals("east") || direction.equals("este")) {
             nextRoom = currentRoom.eastExit;
         }
-        if(direction.equals("south")) {
+        if(direction.equals("south")|| direction.equals("sur")) {
             nextRoom = currentRoom.southExit;
         }
-        if(direction.equals("west")) {
+        if(direction.equals("west") || direction.equals("oeste")) {
             nextRoom = currentRoom.westExit;
         }
 
         if(nextRoom == null){
-            System.out.println("There is no door!");
+            System.out.println(jsonObject.get("Not_door").toString());
         } else {
             currentRoom = nextRoom;
-            System.out.println("You are " + currentRoom.getDescription());
-            System.out.print("Exits: ");
+            System.out.println(jsonObject.get("You_are").toString() + currentRoom.getDescription());
+            System.out.print(jsonObject.get("Exits").toString());
             if(currentRoom.northExit != null) {
-                System.out.print("north ");
+                System.out.print(jsonObject.get("North").toString());
             }
             if(currentRoom.eastExit != null) {
-                System.out.print("east ");
+                System.out.print(jsonObject.get("East").toString());
             }
             if(currentRoom.southExit != null) {
-                System.out.print("south ");
+                System.out.print(jsonObject.get("South").toString());
             }
             if(currentRoom.westExit != null) {
-                System.out.print("west ");
+                System.out.print(jsonObject.get("West").toString());
             }
             System.out.println();
         }
@@ -203,11 +228,20 @@ public class Game
     private boolean quit(Command command) 
     {
         if(command.hasSecondWord()) {
-            view.dangerMsg("Quit what?\n");
+            view.dangerMsg(jsonObject.get("Quit").toString());
             return false;
         }
         else {
             return true;  // signal that we want to quit
+        }
+    }
+
+    private JSONObject readConfig() {
+        try {
+            String jsonContent = Files.readString(Paths.get(source_languaje_path), StandardCharsets.UTF_8);
+            return (JSONObject) new JSONParser().parse(jsonContent);
+        } catch (Exception e) {
+            return new JSONObject();
         }
     }
 }
