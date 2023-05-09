0061751cd82a227beb299cc79bd15e839c7d1c80
Dado00 <62186281+Dado00@users.noreply.github.com>
2023-05-09 15:04:28 -0600

    Update Room.java

--- src/Room.java
+++ src/Room.java
@@ -1,63 +1,89 @@
-package src;
-/**
- * Class Room - a room in an adventure game.
- *
- * This class is part of the "World of Zuul" application. 
- * "World of Zuul" is a very simple, text based adventure game.  
- *
- * A "Room" represents one location in the scenery of the game.  It is 
- * connected to other rooms via exits.  The exits are labelled north, 
- * east, south, west.  For each direction, the room stores a reference
- * to the neighboring room, or null if there is no exit in that direction.
- * 
- * @author  Michael Kolling and David J. Barnes
- * @version 2008.03.30
- */
-public class Room 
-{
-    public String description;
-    public Room northExit;
-    public Room southExit;
-    public Room eastExit;
-    public Room westExit;
+package model;
 
-    /**
-     * Create a room described "description". Initially, it has
-     * no exits. "description" is something like "a kitchen" or
-     * "an open court yard".
-     * @param description The room's description.
-     */
-    public Room(String description) 
-    {
-        this.description = description;
+public class Room extends StateRoom {
+    @Override
+    public boolean isNull() {
+        return false;
     }
 
-    /**
-     * Define the exits of this room.  Every direction either leads
-     * to another room or is null (no exit there).
-     * @param north The north exit.
-     * @param east The east east.
-     * @param south The south exit.
-     * @param west The west exit.
-     */
-    public void setExits(Room north, Room east, Room south, Room west) 
-    {
-        if(north != null)
-            northExit = north;
-        if(east != null)
-            eastExit = east;
-        if(south != null)
-            southExit = south;
-        if(west != null)
-            westExit = west;
+    @Override
+    public boolean accesible() {
+        return true;
     }
 
-    /**
-     * @return The description of the room.
-     */
-    public String getDescription()
-    {
-        return description;
+    @Override
+    public String moveNorth(Model model) {
+        String output;
+
+        if (canGo(getNorth())) {
+            model.setCurrentRoom(getNorth());
+            output = model.getRoomDescription();
+        } else {
+            output = getDefaultUnreachableMessage() + model.getRoomDescription();
+        }
+
+        return output;
     }
 
+    @Override
+    public String moveSouth(Model model) {
+        String output;
+
+        if (canGo(getSouth())) {
+            model.setCurrentRoom(getSouth());
+            output = model.getRoomDescription();
+        } else {
+            output = getDefaultUnreachableMessage() + model.getRoomDescription();
+        }
+
+        return output;
+    }
+
+    @Override
+    public String moveEast(Model model) {
+        String output;
+
+        if (canGo(getEast())) {
+            model.setCurrentRoom(getEast());
+            output = model.getRoomDescription();
+        } else {
+            output = getDefaultUnreachableMessage() + model.getRoomDescription();
+        }
+
+        return output;
+    }
+
+    @Override
+    public String moveWest(Model model) {
+        String output;
+
+        if (canGo(getWest())) {
+            model.setCurrentRoom(getWest());
+            output = model.getRoomDescription();
+        } else {
+            output = getDefaultUnreachableMessage() + model.getRoomDescription();
+        }
+
+        return output;
+    }
+
+    public void connectNorth(Room room) {
+        this.setNorth(room);
+        room.setSouth(this);
+    }
+    
+    public void connectSouth(Room room) {
+        this.setSouth(room);
+        room.setNorth(this);
+    }
+    
+    public void connectEast(Room room) {
+        this.setEast(room);
+        room.setWest(this);
+    }
+    
+    public void connectWest(Room room) {
+        this.setWest(room);
+        room.setEast(this);
+    }
 }
