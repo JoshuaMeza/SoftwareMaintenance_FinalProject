8cb7fa972441dd0f50715933ca2042d7609c4032
Joshua Meza <56287951+JoshuaMeza@users.noreply.github.com>
2023-05-09 13:26:35 -0600

    MZ-4-20230507 (#1)
    Co-authored-by: JonathanGGB <56163813+JonathanGGB@users.noreply.github.com>

--- src/Game.java
+++ src/Game.java
@@ -163,22 +163,26 @@ public class Game
             nextRoom = currentRoom.westExit;
         }
 
-        currentRoom = nextRoom;
-        System.out.println("You are " + currentRoom.getDescription());
-        System.out.print("Exits: ");
-        if(currentRoom.northExit != null) {
-            System.out.print("north ");
+        if(nextRoom == null){
+            System.out.println("There is no door!");
+        } else {
+            currentRoom = nextRoom;
+            System.out.println("You are " + currentRoom.getDescription());
+            System.out.print("Exits: ");
+            if(currentRoom.northExit != null) {
+                System.out.print("north ");
+            }
+            if(currentRoom.eastExit != null) {
+                System.out.print("east ");
+            }
+            if(currentRoom.southExit != null) {
+                System.out.print("south ");
+            }
+            if(currentRoom.westExit != null) {
+                System.out.print("west ");
+            }
+            System.out.println();
         }
-        if(currentRoom.eastExit != null) {
-            System.out.print("east ");
-        }
-        if(currentRoom.southExit != null) {
-            System.out.print("south ");
-        }
-        if(currentRoom.westExit != null) {
-            System.out.print("west ");
-        }
-        System.out.println();
     }
 
     /** 
